4caf9f623bbfc8ab90639cba6f0bbe34588da027
AdelRomeo <11cage11@mail.ru>
2021-05-31 21:25:27 +0700

    визуализация ответов

--- src/containers/Quiz/Quiz.js
+++ src/containers/Quiz/Quiz.js
@@ -8,26 +8,96 @@ import classes from './Quiz.module.scss';
 export default class Quiz extends Component {
 
   state = {
+    activeQuestion: 0,
+    //правильынй или не правильный ответ
+    answerState: null,
     //список вопросов
     quiz: [
       {
+        //вопрос
+        question: 'Какого цвета небо?',
+        //вариант правильного ответа
+        rightAnswerId: 2,
+        id: 1,
+        //варианты ответа
         answers: [
-          {text: 'Вопрос1'},
-          {text: 'Вопрос2'},
-          {text: 'Вопрос3'},
-          {text: 'Вопрос4'},
+          {text: 'Черный', id: 1},
+          {text: 'Синий', id: 2},
+          {text: 'Красный', id: 3},
+          {text: 'Зеленый', id: 4}
+        ]
+      },
+      {
+        //вопрос
+        question: 'Какое число больше?',
+        //вариант правильного ответа
+        rightAnswerId: 3,
+        id: 2,
+        //варианты ответа
+        answers: [
+          {text: 20, id: 1},
+          {text: 33, id: 2},
+          {text: 129, id: 3},
+          {text: 7, id: 4}
         ]
       }
     ]
   }
 
+  //клики по вариантам ответа
+  onAnswerClickHandler = answerId => {
+
+    //активный вопрос
+    const question = this.state.quiz[this.state.activeQuestion];
+
+    //если правильный вариант равен варианту ответа на который кликнули
+    if (question.rightAnswerId === answerId) {
+      //ставим маркер 'правильный ответ'
+      this.setState({
+        answerState: {[answerId]: 'success'}
+      })
+      //timeout для визуализации
+      const timeout = window.setTimeout(() => {
+        //закончились ли вопросы
+        if (this.isQuizFinished()) {
+          console.log('Finished')
+        } else {
+          //переходим к следующему вопросу
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        //убираем timeout
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      //ставим маркер 'не правильный ответ'
+      this.setState({
+        answerState: {[answerId]: 'error'}
+      })
+    }
+  }
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
+  }
+
   render() {
+
+    const {quiz, activeQuestion, answerState} = this.state
+
     return (
       <div className={classes.Quiz}>
         <div className={classes.QuizWrapper}>
           <h1>Ответьте на все вопросы</h1>
           <ActiveQuiz
-            answers={this.state.quiz[0].answers}
+            answers={quiz[activeQuestion].answers}
+            question={quiz[activeQuestion].question}
+            quizLength={quiz.length}
+            onAnswerClick={this.onAnswerClickHandler}
+            answerNumber={activeQuestion + 1}
+            state={answerState}
           />
         </div>
       </div>

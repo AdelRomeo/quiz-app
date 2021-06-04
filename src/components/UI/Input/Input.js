5ee82200ca81086b87d54120a6540bd964f22d03
AdelRomeo <11cage11@mail.ru>
2021-06-04 20:51:35 +0700

    form v1

---
+++ src/components/UI/Input/Input.js
@@ -0,0 +1,40 @@
+import React from 'react';
+//стили
+import classes from './Input.module.scss';
+
+const Input = (props) => {
+
+  //список классов
+  const cls = [
+    classes.Input
+  ]
+
+  //генерация id
+  const htmlFor = `${props.type}-${Math.random()}`
+
+  if (isInvalid(props)){
+    cls.push(classes.invalid)
+  }
+
+  return (
+    <div className={cls.join(' ')}>
+      <label htmlFor={htmlFor}>{props.label}</label>
+      <input
+        type={props.type || 'text'}
+        id={props.htmlFor}
+        value={props.value}
+        onChange={props.onChange}
+      />
+      { isInvalid(props) ? <span>{props.errorMessage || 'Введите верное значение'}</span> : null }
+
+    </div>
+  )
+}
+
+//проверка валидности инпута
+const isInvalid = ({valid, touched, shouldValidate}) => {
+  //возвращаем true если input не валидный и он должен валидироваться и с ним уже взаимодействовали
+  return !valid && shouldValidate && touched
+}
+
+export default Input
